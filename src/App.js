b61c957b397872d8eb3cbc186b6712225ce11921
Shankar <shankar.tiwari@cayuse.com>
2023-02-11 22:03:12 -0800

    final changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [text, setText] = useState([]);
+    const [show, setShow] = useState(false);
+    let textRef = React.createRef();
+
+    const saveNote = () => {
+        let inputVal = textRef.current.value;
+        if(inputVal && !text.includes(inputVal)) {
+            setText(prevText => prevText ? prevText.concat(inputVal) : [inputVal])
+            setShow(true);
+        }
+
+        else if(inputVal && text.includes(inputVal)){
+            alert('duplicate note')
+        }
+    }
+
+    const handleClear = () => {
+        textRef.current.value = ""
+        setText("")
+    }
+
+    const deleteNote = (event) => {
+        if(event?.target?.id){
+            setText(text.filter(e => e !== event.target.id.split("_")[1]));
+            textRef.current.value = ""
+        }
+    }
+
+    const showData = () => {
+        return (
+            <div className="noteDiv"> <b>Notes:</b>
+                {show && text.length > 0 && text.map((item, index) => {
+                    return(
+                        <div key={index}>
+                            {item} <button id={index+"_"+item} onClick={deleteNote}>-</button>
+                        </div>
+                    )})
+                }
+            </div>
+        );
+    }
+
+    return (
+        <div className="App">
+          <div className="Main">
+              <div><input ref={textRef} type="text" placeholder="write a note" />
+                  <button onClick={saveNote}>+</button>
+                  <button onClick={handleClear}>Clear Notes</button>
+              </div>
+
+              {showData()}
+          </div>
+        </div>
+   );
 }
 
 export default App;
